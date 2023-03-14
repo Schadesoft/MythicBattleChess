823371ae4dda5f9bb1565fb27d3a62218f7940a4
Schade <30397845+Schadesoft@users.noreply.github.com>
2023-03-14 12:53:42 -0700

    Update MythicBattleChess.py

--- MythicBattleChess.py
+++ MythicBattleChess.py
@@ -66,20 +66,3 @@ def move_piece(board, row, col, new_row, new_col):
     board[row][col] = " "
 
 # Play the game
-
-
-"""
-
-Mythic Battle Chess is a chess-like strategy game that combines elements of Norse and Greek mythology. The game is played on an 8x8 board and features five unique units: Gnome, Valkyrie, Hoplite, Centaur, and Minotaur. Each unit has its unique set of moves, making the game challenging and exciting.
-
-The game is played between two players, one controlling the Greek units, and the other controlling the Norse units. The game objective is to checkmate the opponent's king.
-
-Each player starts with three pieces, and the pieces are placed randomly on the board. The pieces move in different ways, which depend on their unique set of moves. The Gnome moves diagonally, the Valkyrie moves in an L shape, the Hoplite moves forward and backward, the Centaur moves left and right, and the Minotaur can move in any direction and jump over other pieces.
-
-The game follows standard chess rules, such as turns alternating between players, capturing opponent pieces, and checking the king. If a player's king is in check, they must move out of check or block the attack. If a player's king is in checkmate, the game ends, and the opponent wins.
-
-Mythic Battle Chess is a game that requires strategic planning and critical thinking, making it a challenging and exciting game to play.
-
-Overall, Mythic Battle Chess is a fun and engaging game that combines the best of Greek and Norse mythology and chess.
-
-"""
